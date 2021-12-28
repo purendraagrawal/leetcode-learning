70b98d9c70d59cce310492acef85a021875b3e87
Purendra Agrawal <purendra.agrawal@gmail.com>
2021-12-28 08:12:50 +0530

    Time: 4 ms (85.01%), Space: 38.9 MB (28.00%) - LeetHub

--- 3sum-closest/3sum-closest.java
+++ 3sum-closest/3sum-closest.java
@@ -1,27 +1,37 @@
 class Solution {
-    public int threeSumClosest(int[] nums, int target) {   
-     int size = nums.length;
-        int closestSum=Integer.MAX_VALUE;
-        int min = Integer.MAX_VALUE;
-        if(size==3)
-            return nums[0]+nums[1]+nums[2];
-        else{
-            for(int i=0;i<size-2;i++){
-                int first = nums[i];
-                for(int j=i+1;j<size-1;j++){
-                    int second = nums[j];
-                 for(int k = j+1;k<size;k++){
-                     int third = nums[k];
-                     int sum = first+second+third;
-                     int diff = Math.abs(sum-target);
-                    if(min>diff){
-                        closestSum = sum;
-                        min= diff;
-                    }
-                 }   
-                }
-            }
-        }
-        return closestSum;
+    public int threeSumClosest(int[] nums, int target) {
+     Arrays.sort(nums);
+		int size = nums.length;
+		int closestSum = Integer.MAX_VALUE;
+		int min = Integer.MAX_VALUE;
+		if (size == 3)
+			return nums[0] + nums[1] + nums[2];
+		else {
+			for (int i = 0; i < size; i++) {
+				int first = nums[i];
+				int j = i + 1;
+				int k = size - 1;
+				while (j < k) {
+					int second = nums[j];
+					int third = nums[k];
+					int sum = first + second + third;
+					int diff = Math.abs(sum - target);
+					if (min > diff) {
+						closestSum = sum;
+						min = diff;
+						if (diff == 0)
+							break;
+					}
+					if (sum > target)
+						k--;
+					else
+						j++;
+				}
+				if (min == 0)
+					break;
+			}
+
+		}
+		return closestSum;
     }
 }
