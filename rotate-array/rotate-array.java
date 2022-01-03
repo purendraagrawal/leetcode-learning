73cf0dfddabdcb9e4cfc720652c532254c88a88c
Purendra Agrawal <purendra.agrawal@gmail.com>
2022-01-03 21:27:06 +0530

    Time: 1 ms (66.02%), Space: 64.5 MB (6.67%) - LeetHub

---
+++ rotate-array/rotate-array.java
@@ -0,0 +1,18 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+        int len = nums.length;
+        k = k%len;//If k is greater than length, why we have to run it again. Find the remainder and make it k.
+        reverse(nums, 0, len-1);
+        reverse(nums, 0, k-1);
+        reverse(nums,k,len-1);
+    }
+    public void reverse(int[] nums, int start, int end){
+        while(start<end){
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
