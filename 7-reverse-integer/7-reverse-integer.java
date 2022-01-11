a2c1e00f486ebc6537c710bcc1768b81dd967610
Purendra Agrawal <purendra.agrawal@gmail.com>
2022-01-11 08:38:05 +0530

    Time: 2 ms (44.16%), Space: 38.2 MB (19.26%) - LeetHub

--- 7-reverse-integer/7-reverse-integer.java
+++ 7-reverse-integer/7-reverse-integer.java
@@ -1,22 +1,33 @@
 class Solution {
-    public int reverse(int x) {
-     int reverse = 0;
-     int temp = x;
-        boolean negative = false;
-        if(temp<0){
-            negative = true;
-            temp=temp*-1;
+    public int reverse(int x) 
+    {
+        int n = x;
+        long flag = 0, rev = 0;
+		
+		// Checking wether the number is neagtive
+		// * If negative make it positive by multiplying by " -1 "
+		// * The flag is a reference if the number (x) is negative.
+        if (x < 0) 
+        {
+            flag = 1;
+            n = -1*n;
         }
-        while(temp>0){
-            int r = temp % 10;
-            if(reverse > Integer.MAX_VALUE/10 || reverse == Integer.MAX_VALUE/10 && temp >7)
-                return 0;
-            reverse = reverse*10+r;
-            int q = temp/10;
-            temp = q;
+		// Reversing the Integer by using mod 
+        while (n > 0)
+        {
+            rev = rev * 10 + n % 10;
+            n = n/ 10;
         }
-        if(negative)
-            reverse = reverse * -1;
-        return reverse;
-    }
+		
+        // If the reversed Integer is greater/lesser than Integer bounds (i.e [−2^31,  2^31 − 1]) then we return 0.
+         if(rev > Integer.MAX_VALUE || rev < Integer.MIN_VALUE  ) 
+            return 0;
+			
+        // If not the above condition if the number (x) is negative,
+        // we multiply the reversed number (rev) by " -1 " to make it negative again and return it.
+         else if (flag != 0)
+            return -1 *(int) rev;
+         else
+            return (int)rev;
+    } 
 }
