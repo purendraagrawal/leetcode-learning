ae3640d5df045308cd5a18f815c38fad5d3dac96
Purendra Agrawal <purendra.agrawal@gmail.com>
2022-01-11 08:06:03 +0530

    Time: 14 ms (35.77%), Space: 45.1 MB (27.91%) - LeetHub

---
+++ 6-zigzag-conversion/6-zigzag-conversion.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String convert(String s, int numRows) {
+     StringBuilder[] sb = new StringBuilder[numRows];
+        for(int i=0;i<numRows;i++){
+            sb[i]=  new StringBuilder();
+        }
+        int i = 0;
+        int len = s.length();
+        while(i<len){
+            for(int j=0; j < numRows && i<len;j++){
+                sb[j] = sb[j].append(s.charAt(i));
+                i++;
+            }
+            for(int j=numRows-2;j>=1 && i < len;j--){
+                sb[j] = sb[j].append(s.charAt(i));
+                i++;
+            }
+        }
+        for(int j=1;j<sb.length;j++){
+            sb[0] = sb[0].append(sb[j]);
+        }
+        return sb[0].toString();
+    }
+}
