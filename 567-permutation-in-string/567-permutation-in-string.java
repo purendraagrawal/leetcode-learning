13d5fa7834656e78bbe5678df6503c25163f3c8f
Purendra Agrawal <purendra.agrawal@gmail.com>
2022-01-15 18:57:43 +0530

    Time: 8 ms (64.10%), Space: 40.2 MB (27.96%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.java
+++ 567-permutation-in-string/567-permutation-in-string.java
@@ -5,18 +5,18 @@ class Solution {
         if(lengthOfS1>lengthOfS2)
             return false;
         int[] s1Arr = new int[26];
+        int[] s2Arr = new int[26];
         for(int i=0;i<lengthOfS1;i++){
             s1Arr[s1.charAt(i)-'a']++;
+            s2Arr[s2.charAt(i)-'a']++;
         }
-        for(int i=0;i<=lengthOfS2-lengthOfS1;i++){
-            int[] s2Arr = new int[26];
-            for(int j=0;j<lengthOfS1;j++){
-                s2Arr[s2.charAt(i+j)-'a']++;
-            }
+        for(int i=0;i<lengthOfS2-lengthOfS1;i++){
             if(isMatch(s1Arr, s2Arr))
                 return true;
+            s2Arr[s2.charAt(i+lengthOfS1)-'a']++;
+            s2Arr[s2.charAt(i)-'a']--;
         }
-        return false;
+        return isMatch(s1Arr, s2Arr);
     }
     
     public boolean isMatch(int[] s1Arr, int[] s2Arr){
