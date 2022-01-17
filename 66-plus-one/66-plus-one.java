53016ed3f4361016937d72347e09970459ff7352
Purendra Agrawal <purendra.agrawal@gmail.com>
2022-01-17 06:20:47 +0530

    Time: 1 ms (16.55%), Space: 39 MB (30.27%) - LeetHub

--- 66-plus-one/66-plus-one.java
+++ 66-plus-one/66-plus-one.java
@@ -1,16 +1,16 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-     	int length = digits.length;
-		for (int i = length - 1; i >= 0; i--) {
-			if (digits[i] < 9) {
-				digits[i] = digits[i] + 1;
-				return digits;
-			}
-			digits[i] = 0;
-		}
-
-		int[] digits1 = new int[length + 1];
-		digits1[0] = 1;
-		return digits1;
+     int len = digits.length;
+        for(int i=len-1;i>=0;i--){
+            if(digits[i]==9){
+                digits[i]=0;
+            }else{
+                digits[i]++;
+                return digits;
+            }
+        }
+        int[] digits1 = new int[len+1];
+        digits1[0] = 1;
+        return digits1;
     }
 }
