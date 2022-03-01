b238e7c76ae9750ad055a9e9bdb00fb3eee095ed
Purendra Agrawal <purendra.agrawal@gmail.com>
2022-03-01 20:00:30 +0530

    Time: 0 ms (100.00%), Space: 64.3 MB (42.42%) - LeetHub

--- 189-rotate-array/189-rotate-array.java
+++ 189-rotate-array/189-rotate-array.java
@@ -1,7 +1,7 @@
 class Solution {
     public void rotate(int[] nums, int k) {
         int len = nums.length;
-        k = k % len;
+        k = k % len;//If k is greater than length, why we have to run it again. Find the remainder and make it k.
         reverse(nums, 0, len-1);
         reverse(nums,0,k-1);
         reverse(nums,k,len-1);
