d062a78cfa22c88b8fdd4969922dda861d7b080b
Purendra Agrawal <purendra.agrawal@gmail.com>
2022-03-02 08:56:48 +0530

    Time: 3 ms (40.02%), Space: 54.7 MB (26.88%) - LeetHub

---
+++ 283-move-zeroes/283-move-zeroes.java
@@ -0,0 +1,13 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int start = 0;
+        for(int end = 0;end<nums.length;end++){
+            if(nums[end]!=0){
+                nums[start++] = nums[end];
+            }
+        }
+        while(start<nums.length){
+            nums[start++]=0;
+        }
+    }
+}
