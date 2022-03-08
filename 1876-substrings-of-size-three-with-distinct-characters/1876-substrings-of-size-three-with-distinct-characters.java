459f4d5707a043c63edfa1ff822e2ab1443a7d3d
Purendra Agrawal <purendra.agrawal@gmail.com>
2022-03-08 07:18:49 +0530

    Time: 7 ms (16.30%), Space: 44 MB (7.11%) - LeetHub

---
+++ 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int countGoodSubstrings(String s) {
+       int result = 0;
+        for(int i=0;i<s.length()-2;i++){
+         Set<Character> set = new HashSet<>();
+         set.add(s.charAt(i));
+         set.add(s.charAt(i+1));
+         set.add(s.charAt(i+2));
+         if(set.size()==3)
+             result++;
+        }
+        return result;
+    }
+}
