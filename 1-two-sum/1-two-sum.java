11b2b1628fd538585df555999029f06a53db50b0
Purendra Agrawal <purendra.agrawal@gmail.com>
2022-03-22 19:08:36 +0530

    Time: 8 ms (48.85%), Space: 47.3 MB (5.46%) - LeetHub

--- 1-two-sum/1-two-sum.java
+++ 1-two-sum/1-two-sum.java
@@ -1,13 +1,16 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-     Map<Integer, Integer> map = new HashMap<>();
-        for(int i=0;i<nums.length;i++){
-            int complement = target - nums[i];
-            if(map.containsKey(complement)){
-                return new int[] {i, map.get(complement)};
-            }
-            map.put(nums[i], i);
-        }
-        throw new IllegalArgumentException("No two sum solution");
+       Map<Integer, Integer> map = new HashMap<>();
+       int[] result = new int[2]; 
+       for(int i=0;i<nums.length;i++){
+          int rem = target-nums[i];
+          if(map.containsKey(rem)){
+              result[0] = i;
+              result[1] = map.get(rem);
+          }else{ 
+            map.put(nums[i],i);
+          }
+       }
+       return result; 
     }
 }
