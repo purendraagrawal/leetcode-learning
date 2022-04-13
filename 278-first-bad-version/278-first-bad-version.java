17e79e5859b790b3170110aac519136779b16e3c
Purendra Agrawal <purendra.agrawal@gmail.com>
2022-04-13 08:51:22 +0530

    Time: 14 ms (84.95%), Space: 39 MB (89.46%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.java
+++ 278-first-bad-version/278-first-bad-version.java
@@ -5,14 +5,14 @@ public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
      int start = 1;
         int end = n;
-        while(start<end){
-            int mid = start + (end- start)/2;
+        while(start<=end){
+            int mid = start + (end-start)/2;
             if(isBadVersion(mid)){
-                end = mid;
+                end = mid-1;
             }else{
                 start = mid+1;
             }
         }
-        return end;
+        return start;
     }
 }
