ba9c23991aad234baf3bd60188aca081830a973e
Purendra Agrawal <purendra.agrawal@gmail.com>
2022-05-22 14:25:53 +0530

    Time: 27 ms (50.39%), Space: 48.2 MB (28.60%) - LeetHub

--- 18-4sum/18-4sum.java
+++ 18-4sum/18-4sum.java
@@ -1,55 +1,48 @@
 class Solution {
-    
-    int len = 0;
-    
     public List<List<Integer>> fourSum(int[] nums, int target) {
         Arrays.sort(nums);
-        len = nums.length;
         return kSum(nums, 0, target, 4);
     }
-    public List<List<Integer>> kSum(int[] nums, int index, int target, int k) {
-        List<List<Integer>> result = new ArrayList<>();
-        if (index >= len) {
-            return result;
-        }
-        if (k == 2) {
-            int startIndex = index;
-            int endIndex = len - 1;
-            while (startIndex < endIndex) {
-                int sum = nums[startIndex] + nums[endIndex];
-                if (sum == target) {
-                    List<Integer> twoSumList = new ArrayList<>();
-                    twoSumList.add(nums[startIndex]);
-                    twoSumList.add(nums[endIndex]);
-                    result.add(twoSumList);
-                    // To Skip Duplicates
-                    while (startIndex < endIndex && nums[startIndex] == nums[startIndex + 1])
-                        startIndex++;
-                    while (startIndex < endIndex && nums[endIndex] == nums[endIndex - 1])
-                        endIndex--;
-                    startIndex++;
-                    endIndex--;
-                } else if (sum < target) {
-                    startIndex++;
-                } else {
-                    endIndex--;
-                }
-            }
-        } else {
-            for (int i = index; i < len - k + 1; i++) {
-                List<List<Integer>> kResult = kSum(nums, i + 1, target - nums[i], k - 1);
-                if (kResult != null) {
-                    for (List<Integer> temp : kResult) {
-                        temp.add(0, nums[i]);
+    
+    public List<List<Integer>> kSum(int[] nums, int l, int target, int k){
+        List<List<Integer>> listKSum = new ArrayList<>();
+        int h = nums.length-1;
+        if(k==2){
+            while(l<h){
+                if(nums[l]+nums[h]==target){
+                    List<Integer> list = new ArrayList<>();
+                    list.add(nums[l]);
+                    list.add(nums[h]);
+                    while(l<h && nums[l] == nums[l+1]){
+                        l++;
                     }
-                    result.addAll(kResult);
-                }
-                //To Skip Duplicates
-                while (i < len - 1 && nums[i] == nums[i + 1]) {
-                    i++;
+                    while(l<h && nums[h] == nums[h-1]){
+                        h--;
+                    }
+                    l++;
+                    h--;
+                    listKSum.add(list);
+                }else if(nums[l] + nums[h] < target){
+                    l++;
+                }else{
+                    h--;
                 }
             }
+        }else{
+            for(int i=l;i<nums.length-1;i++){
+            int firstNum = nums[i];
+            List<List<Integer>> res = kSum(nums, i+1 , target-firstNum , k-1);
+            if(!res.isEmpty()){
+                for(List<Integer> list : res){
+                    list.add(firstNum);
+                }
+                listKSum.addAll(res);
+            }
+            while(i<nums.length-1 && nums[i] == nums[i+1]){
+                i++;
+            }
+            }
         }
-        return result;
+        return listKSum;
     }
 }
