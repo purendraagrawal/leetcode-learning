324dae1dc0d95d4506334f058b4875ba7b1f611c
Purendra Agrawal <purendra.agrawal@gmail.com>
2022-07-02 11:23:53 +0530

    Time: 1325 ms (5.18%), Space: 43.8 MB (82.67%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.java
@@ -1,26 +1,29 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
-        int lengthOfS = s.length();
-        int lengthOfP = p.length();
-        List<Integer> list = new ArrayList<>();
-        int[] sArr = new int[26];
-        int[] pArr = new int[26];
-        int i = 0;
-        if(lengthOfS==0 || lengthOfP==0 || lengthOfS<lengthOfP)
-            return list;
-        for(;i<lengthOfP;i++){
-            pArr[p.charAt(i)-97]++;
-            sArr[s.charAt(i)-97]++;
+        List<Integer> result = new ArrayList<>();
+        int lenOfS = s.length();
+        int lenOfP = p.length();
+        if(lenOfS < lenOfP)
+            return result;
+        for(int i =0 ; i <lenOfS-lenOfP+1 ; i++){
+            if(isAnagram(s.substring(i, i+lenOfP),p))
+                result.add(i);
         }
-        for(;i<lengthOfS;i++){
-            int startIndex = i-lengthOfP;
-            if(Arrays.equals(pArr,sArr))
-                list.add(startIndex);
-            sArr[s.charAt(startIndex)-97]--;
-            sArr[s.charAt(i)-97]++;
+        return result;
+    }
+    
+    public boolean isAnagram(String s, String p){
+        int[] arr = new int[26];
+        for(char c : s.toCharArray()){
+            arr[c-97]++;
         }
-        if(Arrays.equals(pArr,sArr))
-                list.add(lengthOfS-lengthOfP);
-        return list;
+        for(char c : p.toCharArray()){
+            arr[c-97]--;
+        }
+        for(int i : arr){
+            if(i!=0)
+                return false;
+        }
+        return true;
     }
 }
